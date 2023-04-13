6100f200076e7ccce14ae25aa70f5f292319e3c8
Dc-Valerian <valerianpedro03@gmail.com>
2023-04-13 13:36:25 +0100

    added a card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Button from './Components/Button'
+import Card from './Components/Card'
 
 const App = () => {
   return (
     <div>
       <Button title='Button' c='white' bg='darkorange' />
+      <Card/>
     </div>
   )
 }
--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div>
+        <Main>
+            <Text>
+                Text
+            </Text>
+            <Field>
+                Field
+            </Field>
+        </Main>
+    </div>
   )
 }
 
 export default Card
+
+const Field = styled.div``
+
+const Text = styled.div`
+
+`
+
+const Main = styled.div`
+width: 400px;
+min-height: 600px;
+border-radius: 10px;
+border-color: silver;
+background-color: green;
+display: flex;
+flex-direction: column;
+align-items: center;
+/* justify-content: center; */
+`
