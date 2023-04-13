3142b578d38a4ecc6ac02df4acb5196d1e15928b
Dc-Valerian <valerianpedro03@gmail.com>
2023-04-13 13:16:02 +0100

    first commit

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Button = () => {
   return (
-    <div>Button</div>
+    <div>
+        <Main bg='red' c='black'>
+            <Text>
+                Button
+            </Text>
+        </Main>
+    </div>
   )
 }
 
 export default Button
+
+const Text = styled.div`
+font-weight: bold;
+
+`
+
+const Main = styled.div<
+{bg:string, c:string}
+>`
+width: 120px;
+height: 80px;
+background-color: ${({bg})=> bg} ;
+color:${({c})=>c};
+border-radius:3px;
+align-items:center;
+justify-content:center;
+transition:all 350ms;
+
+:hover{
+    cursor:pointer;
+    transform:scale(0.97)
+}
+`
